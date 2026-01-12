7e7e6008bcee1df7c66645bdb290c6423325e33b
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2026-01-12 20:50:57 +0200

    feat: update next.config.js for production configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  /* config options here */
+  basePath: isProd ? "/portfolio-2026" : "",
+  assetPrefix: isProd ? "/portfolio-2026/" : "",
+
+  reactStrictMode: true,
 };
 
 export default nextConfig;
